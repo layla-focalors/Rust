93288d64ba50c95db3c1dd9d3b0d2fae4a226989
layla-focalors <starlights2283@gmail.com>
2024-04-17 09:54:49 +0900

    add enum data

---
+++ day2/enums/target/debug/deps/enums.d
@@ -0,0 +1,5 @@
+U:\RustEngine\day2\enums\target\debug\deps\enums.exe: src\main.rs
+
+U:\RustEngine\day2\enums\target\debug\deps\enums.d: src\main.rs
+
+src\main.rs:
---
+++ day2/enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+U:\RustEngine\day2\enums\target\debug\enums.exe: U:\RustEngine\day2\enums\src\main.rs
